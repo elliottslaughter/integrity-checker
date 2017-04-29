627ef8fbf254f445451cfdfa56b71c7f83894989
Elliott Slaughter <elliottslaughter@gmail.com>
2017-04-29 11:48:26 -0700

    Compute hash sum generically.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 extern crate clap;
+extern crate generic_array;
 extern crate ignore;
+
+extern crate digest;
 extern crate sha2;
 
 extern crate integrity_checker;
 
+use std::default::Default;
 use std::ffi::OsString;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
-use sha2::Digest;
+use generic_array::GenericArray;
 use ignore::WalkBuilder;
+use digest::Digest;
 
 use integrity_checker::error;
+
+#[derive(Debug, Clone, PartialEq)]
+struct HashSum<T: Digest>(GenericArray<u8, <T as Digest>::OutputSize>);
 
 fn parse_args() -> OsString {
     let matches = clap::App::new("Integrity Checker")
@@ -24,10 +32,14 @@ fn parse_args() -> OsString {
     matches.value_of_os("path").unwrap().to_owned()
 }
 
-fn compute_hash<P: AsRef<Path>>(path: P) -> Result<String, error::Error> {
+fn compute_hash<D, P>(path: P) -> Result<HashSum<D>, error::Error>
+where
+    D: Digest + Default,
+    P: AsRef<Path>,
+{
     let mut f = File::open(path)?;
 
-    let mut hasher = sha2::Sha256::default();
+    let mut hasher = D::default();
 
     let mut buffer = [0; 4096];
 
@@ -36,15 +48,16 @@ fn compute_hash<P: AsRef<Path>>(path: P) -> Result<String, error::Error> {
         if n == 0 { break }
         hasher.input(&buffer[0..n]);
     }
-    Ok(hasher.result().map(|b| format!("{:02x}", b)).join(""))
+    Ok(HashSum(hasher.result()))
 }
 
 fn walk_directory<P: AsRef<Path>>(path: P) -> Result<(), error::Error> {
     for entry in WalkBuilder::new(path).build() {
         let entry = entry?;
         if entry.file_type().map_or(false, |t| t.is_file()) {
-            let hash = compute_hash(entry.path())?;
-            println!("{}  {}", hash, entry.path().display());
+            let hash: HashSum<sha2::Sha256> = compute_hash(entry.path())?;
+            let result = hash.0.map(|b| format!("{:02x}", b)).join("");
+            println!("{}  {}", result, entry.path().display());
         }
     }
     Ok(())
