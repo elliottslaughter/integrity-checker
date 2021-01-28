d9682219c271fc0991fa1e1a9271a21b39203984
Elliott Slaughter <elliottslaughter@gmail.com>
2021-01-27 21:02:07 -0800

    Fix CI test script.

--- test.sh
+++ test.sh
@@ -13,12 +13,12 @@ cargo test $FEATURES
 cargo bench $FEATURES
 
 # Test command-line interface
-cargo run $FEATURES -- build db README.md
-cargo run $FEATURES -- check db README.md
-cargo run $FEATURES -- build db2 README.md
-( ! cargo run $FEATURES -- build db README.md ) && true || false # shouldn't overwrite
-cargo run $FEATURES -- build db README.md -f # should overwrite
-cargo run $FEATURES -- diff db db2
-cargo run $FEATURES -- selfcheck db
+cargo run $FEATURES -- build db.json.gz README.md
+cargo run $FEATURES -- check db.json.gz README.md
+cargo run $FEATURES -- build db2.json.gz README.md
+( ! cargo run $FEATURES -- build db.json.gz README.md ) && true || false # shouldn't overwrite
+cargo run $FEATURES -- build db.json.gz README.md -f # should overwrite
+cargo run $FEATURES -- diff db.json.gz db2.json.gz
+cargo run $FEATURES -- selfcheck db.json.gz
 gunzip db2.json.gz && echo "asdf" >> db2.json && gzip db2.json
-( ! cargo run $FEATURES -- selfcheck db2 ) && true || false
+( ! cargo run $FEATURES -- selfcheck db2.json.gz ) && true || false
